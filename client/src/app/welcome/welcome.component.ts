10d90a0d80e1941372956dc1323338b9cb584224
carlos-marino <carlos_marinosenra@iescarlesvallbona.cat>
2025-04-07 20:16:07 +0200

    commit para irme

--- client/src/app/welcome/welcome.component.ts
+++ client/src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IJoc } from '../interfaces/iJoc';
 import { DadesJocsService } from '../services/dades-jocs.service';
 import { HttpResponse } from '@angular/common/http';
@@ -11,12 +11,30 @@ import { HttpResponse } from '@angular/common/http';
 })
 export class WelcomeComponent implements OnInit {
   jocs: IJoc[] = [];
+  currentIndex = 0;
 
   constructor(private jocService: DadesJocsService) {}
 
   ngOnInit(): void {
-    this.jocService.getJocs().subscribe((res: HttpResponse<IJoc[]>) => {
-      this.jocs = res.body || [];
+    this.jocService.getJocs().subscribe({
+      next: (res: HttpResponse<IJoc[]>) => {
+        this.jocs = res.body || [];
+      },
+      error: err => {
+        console.error('Error al cargar los jocs:', err);
+      }
     });
   }
+
+  prev(): void {
+    if (this.jocs.length === 0) return;
+    this.currentIndex = (this.currentIndex - 1 + this.jocs.length) % this.jocs.length;
+  }
+
+  next(): void {
+    if (this.jocs.length === 0) return;
+    this.currentIndex = (this.currentIndex + 1) % this.jocs.length;
+  }
 }
+
+
